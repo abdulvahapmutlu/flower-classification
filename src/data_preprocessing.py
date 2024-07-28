a23f1c61f56a796d24450344bfffc987c1082b51
Abdulvahap Mutlu <abdulvahapmutlu1@gmail.com>
2024-07-28 13:48:36 +0300

    Create data_preprocessing.py

---
+++ src/data_preprocessing.py
@@ -0,0 +1,37 @@
+import os
+from torchvision import datasets, transforms
+
+def get_data_loaders(data_dir, batch_size=32):
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                              data_transforms[x])
+                      for x in ['train', 'val', 'test']}
+    
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size,
+                                                  shuffle=True, num_workers=4)
+                   for x in ['train', 'val', 'test']}
+    
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val', 'test']}
+    class_names = image_datasets['train'].classes
+    
+    return dataloaders, dataset_sizes, class_names
